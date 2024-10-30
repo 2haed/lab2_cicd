a24229daaa5e16b4d7a035d467267a9ead1c6c1e
kapus <yurlov.daniill1@yandex.ru>
2024-10-30 12:30:09 +0300

    base_func

---
+++ main.py
@@ -0,0 +1,15 @@
+from docx import Document
+
+def process_file(file_path):
+    if file_path.endswith('.txt'):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+    elif file_path.endswith('.docx'):
+        doc = Document(file_path)
+        content = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+    else:
+        raise ValueError("Не поддерживаемый формат")
+    return content
+
+content = process_file("test_file.txt") 
+print(content)
